c65d4d4bfa47a3ed5aac51dfec8a3579f9b6d348
flakey5 <73616808+flakey5@users.noreply.github.com>
2023-08-19 19:18:24 -0700

    Use Node prettier format file

--- src/worker.ts
+++ src/worker.ts
@@ -1,44 +1,43 @@
 import render, { Env as RenderEnv } from './render2';
 
-export interface Env extends RenderEnv {
-}
+export interface Env extends RenderEnv {}
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env,
-        ctx: ExecutionContext,
-    ): Promise<Response> {
-        if (request.method !== 'GET' && request.method !== 'HEAD') {
-            return new Response(undefined, {
-                status: request.method === 'OPTIONS' ? 200 : 405,
-                headers: { Allow: 'GET' },
-            });
-        }
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    if (request.method !== 'GET' && request.method !== 'HEAD') {
+      return new Response(undefined, {
+        status: request.method === 'OPTIONS' ? 200 : 405,
+        headers: { Allow: 'GET' },
+      });
+    }
 
-        const url = new URL(request.url);
-        if (url.pathname.endsWith('.json')) {
-            env.ALLOWED_ORIGINS = '*';
-        }
+    const url = new URL(request.url);
+    if (url.pathname.endsWith('.json')) {
+      env.ALLOWED_ORIGINS = '*';
+    }
 
-        let r2Path: string = url.pathname;
-        if (url.pathname.startsWith('/dist')) {
-            r2Path = `/nodejs/release${url.pathname.substring(5)}`;
-        } else if (url.pathname.startsWith('/download')) {
-            r2Path = `/nodejs${url.pathname.substring(9)}`;
-        } else if (url.pathname.startsWith('/docs')) {
-            r2Path = `/nodejs/docs${url.pathname.substring(5)}`;
-        } else if (url.pathname.startsWith('/api')) {
-            r2Path = `/nodejs/docs/latest/api${url.pathname.substring(4)}`;
-        } else if (!url.pathname.startsWith('/metrics')) {
-            return new Response(undefined, { status: 401 });
-        }
-        r2Path = decodeURIComponent(r2Path);
+    let r2Path: string = url.pathname;
+    if (url.pathname.startsWith('/dist')) {
+      r2Path = `/nodejs/release${url.pathname.substring(5)}`;
+    } else if (url.pathname.startsWith('/download')) {
+      r2Path = `/nodejs${url.pathname.substring(9)}`;
+    } else if (url.pathname.startsWith('/docs')) {
+      r2Path = `/nodejs/docs${url.pathname.substring(5)}`;
+    } else if (url.pathname.startsWith('/api')) {
+      r2Path = `/nodejs/docs/latest/api${url.pathname.substring(4)}`;
+    } else if (!url.pathname.startsWith('/metrics')) {
+      return new Response(undefined, { status: 401 });
+    }
+    r2Path = decodeURIComponent(r2Path);
 
-        if (r2Path[r2Path.length - 1] != '/' && r2Path.lastIndexOf('.') == -1) {
-            r2Path += '/';
-        }
+    if (r2Path[r2Path.length - 1] != '/' && r2Path.lastIndexOf('.') == -1) {
+      r2Path += '/';
+    }
 
-        return await render.fetch(r2Path, request, env, ctx);
-    },
+    return await render.fetch(r2Path, request, env, ctx);
+  },
 };
