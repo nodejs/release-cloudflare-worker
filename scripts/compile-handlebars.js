3e787846eeac439b59364d4394fb41eccaf347d6
Claudio Wunder <cwunder@hubspot.com>
2023-09-27 22:40:39 +0200

    chore: added some docs

--- scripts/compile-handlebars.js
+++ scripts/compile-handlebars.js
@@ -10,6 +10,10 @@ import Handlebars from 'handlebars';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const removeStringIndents = str =>
+  // This function allows removing all spaces before a < and
+  // all new lines and new lines accompanied by spaces
+  // This is the most simplistic yet effective way to remove
+  // empty spaces and new lines to minify the HTML result for the Response Body
   str.replace(/[ ]+</g, '<').replace(/\\n[ ]*/g, '');
 
 const templatesToParse = glob('src/templates/*.hbs', {
@@ -29,12 +33,16 @@ templatesToParse.then(files => {
       strict: true,
     });
 
+    // The Handlebars.precompile returns a JavaScript object
+    // and then we make a default export of the object
     const javascriptTemplate = `export default ${removeStringIndents(
       compiledTemplate
     )}`;
 
     const outputFilename = filePath.replace('.hbs', '.out.js');
 
+    // We minify the resulting JavaScript file with Terser and
+    // then write on the same directory but with `.out.js` extension
     minify(javascriptTemplate).then(({ code }) =>
       writeFileSync(outputFilename, code, 'utf8')
     );
