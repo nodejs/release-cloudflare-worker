5d94b1c79211d713e9a6a928e899d4ddb1d0d219
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-10-04 19:18:26 -0700

    setup node
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- scripts/promote-release.js
+++ scripts/promote-release.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-import { S3Client, ListObjectsV2Command, CopyObjectCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  ListObjectsV2Command,
+  CopyObjectCommand,
+} from '@aws-sdk/client-s3';
 
 if (process.argv.length !== 3) {
   console.error(`usage: promote-release <prefix in dist-staging to promote>`);
@@ -19,8 +23,8 @@ if (!process.env.CF_SECRET_ACCESS_KEY) {
 
 const ENDPOINT =
   'https://07be8d2fbc940503ca1be344714cb0d1.r2.cloudflarestorage.com';
-const PROD_BUCKET = 'dist-prod'
-const STAGING_BUCKET = 'dist-staging'
+const PROD_BUCKET = 'dist-prod';
+const STAGING_BUCKET = 'dist-staging';
 const RETRY_LIMIT = 3;
 
 const client = new S3Client({
@@ -28,20 +32,20 @@ const client = new S3Client({
   region: 'auto',
   credentials: {
     accessKeyId: process.env.CF_ACCESS_KEY_ID,
-    secretAccessKey: process.env.CF_SECRET_ACCESS_KEY
-  }
-})
+    secretAccessKey: process.env.CF_SECRET_ACCESS_KEY,
+  },
+});
 
-const path = process.argv[2]
+const path = process.argv[2];
 const files = await getFilesToPromote(path);
 
 for (const file of files) {
-  promoteFile(file)
+  promoteFile(file);
 }
 
 /**
  * @param {string} path
- * @returns {string[]} 
+ * @returns {string[]}
  */
 async function getFilesToPromote(path) {
   let paths = [];
@@ -49,56 +53,56 @@ async function getFilesToPromote(path) {
   let truncated = true;
   let continuationToken;
   while (truncated) {
-    const data = await retryWrapper(
-      async () => {
-        return await client.send(
-          new ListObjectsV2Command({
-            Bucket: STAGING_BUCKET,
-            Delimiter: '/',
-            Prefix: path,
-            ContinuationToken: continuationToken
-          })
-        )
-      }
-    )
+    const data = await retryWrapper(async () => {
+      return await client.send(
+        new ListObjectsV2Command({
+          Bucket: STAGING_BUCKET,
+          Delimiter: '/',
+          Prefix: path,
+          ContinuationToken: continuationToken,
+        })
+      );
+    });
 
     if (data.CommonPrefixes) {
       for (const directory of data.CommonPrefixes) {
-        paths.push(...await getFilesToPromote(directory.Prefix))
+        paths.push(...(await getFilesToPromote(directory.Prefix)));
       }
     }
 
     if (data.Contents) {
       for (const object of data.Contents) {
-        paths.push(object.Key)
+        paths.push(object.Key);
       }
     }
 
-    truncated = data.IsTruncated ?? false
-    continuationToken = data.NextContinuationToken
+    truncated = data.IsTruncated ?? false;
+    continuationToken = data.NextContinuationToken;
   }
 
-  return paths
+  return paths;
 }
 
 /**
- * @param {string} file 
+ * @param {string} file
  */
 async function promoteFile(file) {
-  console.log(`Promoting ${file}`)
+  console.log(`Promoting ${file}`);
 
-  await client.send(new CopyObjectCommand({
-    Bucket: PROD_BUCKET,
-    CopySource: `${STAGING_BUCKET}/${file}`,
-    Key: file
-  }))
+  await client.send(
+    new CopyObjectCommand({
+      Bucket: PROD_BUCKET,
+      CopySource: `${STAGING_BUCKET}/${file}`,
+      Key: file,
+    })
+  );
 }
 
 /**
- * @param {() => Promise<T>} request 
+ * @param {() => Promise<T>} request
  * @returns {Promise<T>}
  */
-async function retryWrapper (request, retryLimit) {
+async function retryWrapper(request, retryLimit) {
   let r2Error;
 
   for (let i = 0; i < RETRY_LIMIT; i++) {
@@ -107,9 +111,9 @@ async function retryWrapper (request, retryLimit) {
       return result;
     } catch (err) {
       r2Error = err;
-      process.emitWarning(`error when contacting r2: ${err}`)
+      process.emitWarning(`error when contacting r2: ${err}`);
     }
   }
 
-  throw r2Error
+  throw r2Error;
 }
