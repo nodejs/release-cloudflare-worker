8469bb9106a7fdf240bbb8cea244d2c2a4e1247f
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-11-08 04:41:21 -0800

    src: don't cache non-GET requests
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,13 +8,14 @@ import { SubtitutionMiddleware } from '../middleware/subtituteMiddleware';
 import type { Router } from './router';
 
 export function registerRoutes(router: Router): void {
-  const r2Middleware = cached(new R2Middleware());
+  const r2Middleware = new R2Middleware();
+  const cachedR2Middleware = cached(r2Middleware);
   const originMiddleware = new OriginMiddleware();
 
   router.options('*', [new OptionsMiddleware()]);
 
   router.head('/metrics/?:filePath+', [r2Middleware, originMiddleware]);
-  router.get('/metrics/?:filePath+', [r2Middleware, originMiddleware]);
+  router.get('/metrics/?:filePath+', [cachedR2Middleware, originMiddleware]);
 
   // Register routes for latest releases (e.g. `/dist/latest/`)
   for (const branch in latestVersions) {
@@ -36,16 +37,19 @@ export function registerRoutes(router: Router): void {
   }
 
   router.head('/dist/?:filePath+', [r2Middleware, originMiddleware]);
-  router.get('/dist/?:filePath+', [r2Middleware, originMiddleware]);
+  router.get('/dist/?:filePath+', [cachedR2Middleware, originMiddleware]);
 
   router.head('/download/?:filePath+', [r2Middleware, originMiddleware]);
-  router.get('/download/?:filePath+', [r2Middleware, originMiddleware]);
+  router.get('/download/?:filePath+', [cachedR2Middleware, originMiddleware]);
 
   router.head('/api/?:filePath+', [r2Middleware, originMiddleware]);
-  router.get('/api/?:filePath+', [r2Middleware, originMiddleware]);
+  router.get('/api/?:filePath+', [cachedR2Middleware, originMiddleware]);
 
   router.head('/docs/?:version?/:filePath+?', [r2Middleware, originMiddleware]);
-  router.get('/docs/?:version?/:filePath+?', [r2Middleware, originMiddleware]);
+  router.get('/docs/?:version?/:filePath+?', [
+    cachedR2Middleware,
+    originMiddleware,
+  ]);
 
   router.get('*', [new NotFoundMiddleware()]);
 }
