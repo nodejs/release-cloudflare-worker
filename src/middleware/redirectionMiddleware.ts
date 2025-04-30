bd7ca6d1bf82381566e99c9aae8fb72a87de02bb
Claudio W. <cwunder@gnome.org>
2025-04-30 12:00:05 +0200

    Update redirectionMiddleware.ts

--- src/middleware/redirectionMiddleware.ts
+++ src/middleware/redirectionMiddleware.ts
@@ -1,12 +1,7 @@
 import type { Middleware } from './middleware';
 
 /**
- * Subtitutes a string in a request's url to a different value and sends it
- *  back to the router to be handled again.
- *
- * This is useful for paths like /dist/latest/, where we look for `latest` and
- *  replace it with whatever the latest version is and send it back to be
- *  handled by the /dist route.
+ * Redirects a path to another URL
  */
 export class RedirectionMiddleware implements Middleware {
   targetURL: string;
